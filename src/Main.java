340f8d15b4709ef7960fc580d136d4f9acb83388
soonduck <soonduck3729@gmail.com>
2024-09-25 15:01:34 +0900

    문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4})));
+    }
+
+    public static int[] solution(int[] arr) {
+        int[] answer = new int[arr.length - 1];
+        int min = arr[0];
+
+        if (arr.length == 1) {
+            answer = new int[]{-1};
+            return answer;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+
+        int num = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != min) {
+                answer[num++] = arr[i];
+            }
+        }
+        return answer;
     }
 }
